60b2f8ecfcf37e7284d7ca093b83d20d5ee730b9
James Widmer <105995913+JamesW6@users.noreply.github.com>
2026-08-06 15:54:11 +0000

    Add: two-integer-sum-ii - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-10.py
@@ -0,0 +1,23 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        update=False
+        for i in range(len(numbers)):
+            num=numbers[i]
+            other_num=self.binSearch(numbers, target-num, i)
+            if  other_num !=-1 and i+1!=other_num+1:
+                return [i+1, other_num+1]
+                
+    def binSearch(self, numbers, target, avoid_index):
+        left=0
+        right=len(numbers)-1
+        while left<=right:
+            mid = (left+right)//2
+            if mid==avoid_index:
+                mid+=1
+            if numbers[mid]==target:
+                return mid
+            elif numbers[mid]<target:
+                left=mid+1
+            else:
+                right=mid-1
+        return -1
